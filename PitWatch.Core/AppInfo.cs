276d1d27a7925d86f45c571b5397daa6b0fcd1ae
szuper1212856 <djbazsibuissnes@gmail.com>
2026-08-28 19:04:01 +0200

    Delete PitWatch.Core directory

--- PitWatch.Core/AppInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PitWatch;
-
-/// <summary>Single place the version string lives, so About/log/README can't disagree.</summary>
-public static class AppInfo
-{
-    public const string Version = "1.0.0";
-    public const string Name = "PitWatch";
-    public const string Tagline = "AI Race Engineer for ACC & LMU";
-}
